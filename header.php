b0d5e37018915c81ff23f87b8667938c66cafc70
devzons <devzons@gmail.com>
2018-07-05 11:39:39 -0700

    First Commit

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>DF Start Theme</title>
+
+	<?php wp_head(); ?>
+
+</head>
+
+<?php // add custom class on specific page
+
+	if( is_front_page() ) :
+		$dfstart_classes = array( 'df-class', 'my-class' );
+	else:
+		$dfstart_classes = array();
+	endif;
+ ?>
+
+<body <?php body_class( $dfstart_classes ); ?>>
+
+	<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
